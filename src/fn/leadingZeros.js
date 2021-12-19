abe91123ca766afe617445a67c26919ca560a576
dasiux <me@siux.info>
2021-12-19 18:14:11 +0100

    add reverse option to leadingZeros

--- src/fn/leadingZeros.js
+++ src/fn/leadingZeros.js
@@ -3,12 +3,13 @@
  * @param {number} num - Number to add prefix
  * @param {number} length - Expected length
  * @param {string} char - Custom character
+ * @param {boolean} reverse - Reverse and
  * @return {string} - The prefixed number
  */
-module.exports = function leadingZeros( num, length = 2, char = '0' ) {
+module.exports = function leadingZeros( num, length = 2, char = '0', reverse = false ) {
     num = num + '';
     while ( num.length < length ) {
-        num = char + num;
+        num = reverse ? num + char : char + num;
     }
     return num;
 };
